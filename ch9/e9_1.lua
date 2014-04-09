4308825b7c13a81b6058d19ae2c2ac3c341f8b41
Ryan Hartlage <ryanplusplus@gmail.com>
2014-04-08 21:31:23 -0400

    Some of chapter 9 done

---
+++ ch9/e9_1.lua
@@ -0,0 +1,55 @@
+function rest(list)
+  return (function(head, ...) return table.pack(...) end)(unpack(list))
+end
+
+function combination_generator(a, m)
+  local tail = rest(a)
+  local result = {}
+
+  if m > #a then
+    return
+  end
+
+  if m == 0 then
+    coroutine.yield({{}})
+    return
+  end
+
+  for v in combinations(tail, m - 1) do
+    table.insert(v, 1, a[1])
+    coroutine.yield(v)
+  end
+
+  for v in combinations(tail, m) do
+    coroutine.yield(v)
+  end
+end
+
+function combinations(a, m)
+  local co = coroutine.create(function() combination_generator(a, m) end)
+
+  return function()
+    local code, res = coroutine.resume(co)
+    return res
+  end
+end
+
+function printResult(a)
+  for i = 1, #a do
+    if type(a[i]) == 'string' then
+      io.write(a[i], ' ')
+    end
+  end
+
+  io.write('\n')
+end
+
+for c in combinations({'a', 'b', 'c'}, 2) do
+  printResult(c)
+end
+
+--[[
+a b
+a c
+c b
+]]
