5e0375522a518adc509631c1354a5ca28f6a461a
ryanplusplus <ryanplusplus@gmail.com>
2014-04-27 16:40:58 -0400

    Ex. 21.1

---
+++ ch21/e21_1.lua
@@ -0,0 +1,28 @@
+--[[
+Write a split function that receives a string and a delimiter pattern and returns a sequence with the chunks in the original string separated by the delimiter:
+
+t = split("a whole new world", " ")
+-- t = {"a", "whole", "new", "world"}
+]]
+
+function split(s, delimiter)
+  res = {}
+  for word in string.gmatch(s, "[^" .. delimiter .. "]+") do
+    res[#res + 1] = word
+  end
+  return res
+end
+
+function printres(res)
+  io.write("{")
+  for i, v in ipairs(res) do
+    io.write('"' .. v .. '"')
+    if i < #res then io.write(", ") end
+  end
+  print("}")
+end
+
+printres(split("a whole new world", " ")) --> {"a", "whole", "new", "world"}
+printres(split("a;whole;new;world", " ")) --> {"a whole new world"}
+printres(split("a;whole;new;world", ";")) --> {"a", "whole", "new", "world"}
+printres(split("a9whole;new7world", "%d;")) --> {"a", "whole", "new", "world"}
