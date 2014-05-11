d1bec1dfb6657343513c415856ec269fbf00029b
Ryan Hartlage <ryanplusplus@gmail.com>
2014-05-11 15:24:48 -0400

    Cleaned up optimization

--- ch24/breakpoint.lua
+++ ch24/breakpoint.lua
@@ -3,39 +3,43 @@ breakpoint = {}
 local breakpoints = {}
 
 local function hook(type)
-  local info
   if type == 'return' then
-    info = debug.getinfo(3, 'fl')
+    local info = debug.getinfo(3, 'fn')
     if info == nil then return end -- Bail out if returning from main
-  else
-    info = debug.getinfo(2, 'fl')
-  end
-  local fn, line = info.func, info.currentline
-  if breakpoints[fn] then
-    if breakpoints[fn][line] and type == 'line' then
+    if breakpoints[info.func] then
+      debug.sethook(hook, 'clr')
+    else
+      debug.sethook(hook, 'cr')
+    end
+  elseif type == 'call' then
+    local info = debug.getinfo(2, 'fn')
+    if breakpoints[info.func] then
+      debug.sethook(hook, 'clr')
+    else
+      debug.sethook(hook, 'cr')
+    end
+  elseif type == 'line' then
+    local info = debug.getinfo(2, 'fln')
+    local fn, line = info.func, info.currentline
+    if breakpoints[fn] and breakpoints[fn][line] then
       debug.debug()
     end
-    debug.sethook(hook, 'clr')
-  else
-    debug.sethook(hook, 'cr')
   end
 end
 
 function breakpoint.set(fn, line)
   breakpoints[fn] = breakpoints[fn] or {}
   breakpoints[fn][line] = true
-  if fn == debug.getinfo(2, 'f').func then debug.sethook(hook, 'clr') end
   return {fn = fn, line = line}
 end
 
 function breakpoint.remove(handle)
   breakpoints[handle.fn][handle.line] = nil
+  local canremove = true
+  for _ in pairs(breakpoints[handle.fn]) do canremove = false end
+  if canremove then breakpoints[handle.fn] = {} end
 end
 
-debug.sethook(hook, 'clr')
+debug.sethook(hook, 'cr')
 
 return breakpoint
-
---[[
-This is kind of a mess.
-]]
