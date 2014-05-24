c7d9f7c1b36b0d31d823e630eb7b0a680b48681d
ryanplusplus <ryanplusplus@gmail.com>
2014-05-24 10:45:03 -0400

    Actually implemented this properly -- previously it used a function instead of a table to do the transliteration

--- ch21/e21_3.lua
+++ ch21/e21_3.lua
@@ -2,8 +2,8 @@
 Write a transliterate function.  This function receives a string and replaces each character in that string by another character, according to a table given as a second argument.  If the table maps 'a' to 'b', the function should replace any occurrence of 'a' by 'b'.  If the table maps 'a' to false, the function should remove occurrences of 'a' from the resulting string.
 ]]
 
-function transliterate(s, f)
-  return (string.gsub(s, "(.)", f))
+function transliterate(s, t)
+  return (string.gsub(s, "(.)", function(c) return t[c] or '' end))
 end
 
-print(transliterate("hi._", function(c) return ({h = 'l', i = 'o', ['.'] = '!'})[c] or '' end)) --> "lo!"
+print(transliterate("hi._", {h = 'l', i = 'o', ['.'] = '!'})) --> "lo!"
