e30b1ca09437b2578f6212fe26218c17dec475da
ryanplusplus <ryanplusplus@gmail.com>
2014-05-24 11:55:20 -0400

    Added a few clarifications

--- ch28/e28_6.lua
+++ ch28/e28_6.lua
@@ -3,9 +3,9 @@ Do you think it is a good design to keep the transliteration table as part of th
 ]]
 
 --[[
-No, this forces clients of the library to coordinate with each other to ensure that the hidden state still has its expected value.  Alternately, each client could set the state each time it uses the transliterate function, but this defeats the purpose of storing the state internally.
+No, not as in the previous two examples.  This forces clients of the library to coordinate with each other to ensure that the hidden state still has its expected value.  Alternately, each client could set the state each time it uses the transliterate function, but this defeats the purpose of storing the state internally.
 
-Instead, it would be better to use a quick closure to achieve the desired behavior:
+Instead, it would be better to use a quick closure to achieve the desired behavior (shown in Lua, but could be done in C):
 
 function specializedtransliterate(t)
   return function(s) transliterate(s, t) end
